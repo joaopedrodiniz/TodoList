a562329a9f94b5d60d684b3fd7e0495fd3789253
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2023-09-01 11:49:19 -0300

    feat: add componente

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <ListTodo />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import ListTodo from './components/ListTasks.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ListTodo,
   }
 });
 </script>
 
 <style>
+
+*{
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+}
+body {
+  background: #222222;;
 }
 </style>
