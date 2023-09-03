4f3a8f3a53bcdda2dddc329859a8d1bf2af57c0d
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2023-09-03 13:49:30 -0300

    feat: add scroll na lista de tarefas

--- src/components/ListTasks.vue
+++ src/components/ListTasks.vue
@@ -9,8 +9,7 @@
     <button v-on:click="getTask()">Criar</button>
   </div>
   
-  <ul class="listTasks">
-    
+  <ul class="listTasks"> 
     <div class="taskInfo">
       <div class="count">
         <span>Tarefas Criadas</span>
@@ -21,16 +20,17 @@
         <span class="countLength">{{countCompletedTasks()}} de {{ listTasks.length }}</span>
       </div>
     </div>
-    
-    <li v-for="task in listTasks" :key="task.id"> 
-      <input type="checkbox" class="buttonDone" v-model="task.done" @change="updateTaskCompletion(task)" />
-      <p :class="{'done': task.done}">{{ task.task }}</p> 
-      <button class="buttonDelete" v-on:click="deleteTask(task)">
-        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0,0,256,256">
-          <g fill="#757575" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(2,2)"><path d="M49,1c-1.66,0 -3,1.34 -3,3c0,1.66 1.34,3 3,3h30c1.66,0 3,-1.34 3,-3c0,-1.66 -1.34,-3 -3,-3zM24,15c-7.17,0 -13,5.83 -13,13c0,7.17 5.83,13 13,13h77v63c0,9.37 -7.63,17 -17,17h-40c-9.37,0 -17,-7.63 -17,-17v-52c0,-1.66 -1.34,-3 -3,-3c-1.66,0 -3,1.34 -3,3v52c0,12.68 10.32,23 23,23h40c12.68,0 23,-10.32 23,-23v-63.35937c5.72,-1.36 10,-6.50062 10,-12.64062c0,-7.17 -5.83,-13 -13,-13zM24,21h80c3.86,0 7,3.14 7,7c0,3.86 -3.14,7 -7,7h-80c-3.86,0 -7,-3.14 -7,-7c0,-3.86 3.14,-7 7,-7zM50,55c-1.66,0 -3,1.34 -3,3v46c0,1.66 1.34,3 3,3c1.66,0 3,-1.34 3,-3v-46c0,-1.66 -1.34,-3 -3,-3zM78,55c-1.66,0 -3,1.34 -3,3v46c0,1.66 1.34,3 3,3c1.66,0 3,-1.34 3,-3v-46c0,-1.66 -1.34,-3 -3,-3z"></path></g></g>
-        </svg>
-    </button>
-    </li>
+    <div class="contentTask">
+      <li v-for="task in listTasks" :key="task.id"> 
+        <input type="checkbox" class="buttonDone" v-model="task.done" @change="updateTaskCompletion(task)" />
+        <p :class="{'done': task.done}">{{ task.task }}</p> 
+        <button class="buttonDelete" v-on:click="deleteTask(task)">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0,0,256,256">
+            <g fill="#757575" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(2,2)"><path d="M49,1c-1.66,0 -3,1.34 -3,3c0,1.66 1.34,3 3,3h30c1.66,0 3,-1.34 3,-3c0,-1.66 -1.34,-3 -3,-3zM24,15c-7.17,0 -13,5.83 -13,13c0,7.17 5.83,13 13,13h77v63c0,9.37 -7.63,17 -17,17h-40c-9.37,0 -17,-7.63 -17,-17v-52c0,-1.66 -1.34,-3 -3,-3c-1.66,0 -3,1.34 -3,3v52c0,12.68 10.32,23 23,23h40c12.68,0 23,-10.32 23,-23v-63.35937c5.72,-1.36 10,-6.50062 10,-12.64062c0,-7.17 -5.83,-13 -13,-13zM24,21h80c3.86,0 7,3.14 7,7c0,3.86 -3.14,7 -7,7h-80c-3.86,0 -7,-3.14 -7,-7c0,-3.86 3.14,-7 7,-7zM50,55c-1.66,0 -3,1.34 -3,3v46c0,1.66 1.34,3 3,3c1.66,0 3,-1.34 3,-3v-46c0,-1.66 -1.34,-3 -3,-3zM78,55c-1.66,0 -3,1.34 -3,3v46c0,1.66 1.34,3 3,3c1.66,0 3,-1.34 3,-3v-46c0,-1.66 -1.34,-3 -3,-3z"></path></g></g>
+          </svg>
+        </button>
+      </li>
+    </div>  
     <h2 class="emptyTask" v-if="listTasks.length === 0">A lista de tarefas está vazia</h2>
   </ul>
   
@@ -145,11 +145,19 @@ export default defineComponent({
     gap: 0.5rem;
     padding: 0;
     padding-top: 1rem;
-
     width: 50%;
     border-top: 1px solid #353535;
     border-radius: 0.3rem;
     
+  }
+
+  .contentTask {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    overflow-y: auto;
+    height: 26rem;
+    padding-right: 0.2rem
   }
 
   .count {
@@ -247,6 +255,20 @@ input[type=checkbox]:checked:after {
   border-radius: 1rem;
   color: white;
   right: -0.2rem;
+}
+
+.contentTask::-webkit-scrollbar {
+  width: 12px;               /* width of the entire scrollbar */
+}
+
+.contentTask::-webkit-scrollbar-track {
+  background: #555555;       /* color of the tracking area */
+}
+
+.contentTask::-webkit-scrollbar-thumb {
+  background-color: white;   /* color of the scroll thumb */
+  border-radius: 20px;       /* roundness of the scroll thumb */
+  border: 3px solid #555555;  /* creates padding around scroll thumb */
 }
 
 @media (max-width: 600px) {
